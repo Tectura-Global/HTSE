5c518db2fdaa080ef05d3c84f72af7491ee113b8
Kgosi <rhulani@tecturaglobal.com>
2026-05-22 12:22:47 +0200

    unit use fixes

--- src/sections/CTA.tsx
+++ src/sections/CTA.tsx
@@ -34,9 +34,9 @@ function CTA () {
     const headerRef = useRef(null)
     const cardsRef = useRef(null)
 
-    const isInView = useInView(listRef, { once: true, margin: "-50vh" })
-    const headerInView = useInView(headerRef, { once: true, margin: "-50vh" })
-    const cardsInView = useInView(cardsRef, { once: true, margin: "-50vh" })
+    const isInView = useInView(listRef, { once: true, margin: "-500px" })
+    const headerInView = useInView(headerRef, { once: true, margin: "-500px" })
+    const cardsInView = useInView(cardsRef, { once: true, margin: "-500px" })
 
     return (
         <section id="cta">
